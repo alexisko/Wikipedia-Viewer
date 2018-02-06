a16a68b7cd1d118f10d027d4240ef28ec8b3e42d
Alexis <alexisko@hawaii.edu>
2018-02-05 21:43:35 -1000

    Got results to append to page

--- js/main.js
+++ js/main.js
@@ -4,21 +4,18 @@ var input = document.getElementById('input');
 $(document).keypress(function(e) {
   var keycode = e.key;
   if (keycode === 'Enter') {
-    console.log(keycode);
-    console.log(input.value);
     getResults(input.value);
   }
 });
 
 // USER PRESSED SEARCH BTN
 $('#search').click(function() {
-  console.log(input.value);
   getResults(input.value);
 });
 
 // RANDOM BTN
 $('#random').click(function() {
-  window.location = "https://en.wikipedia.org/wiki/Special:Random";
+  window.open("https://en.wikipedia.org/wiki/Special:Random");
 });
 
 // AJAX CALL
@@ -34,8 +31,22 @@ function getResults(input) {
           format: 'json'
         },
         dataType: 'jsonp',
-        success: function(data) {
-          console.log(data);
-        }
+        success: displayResults
       });
+}
+
+function displayResults(data) {
+  console.log(data);
+  var results = data.query.search;
+  if(results.length > 0) {
+    for(var i = 0; i < 3; i++) {
+      var title = '<div><a href="https://en.wikipedia.org/wiki/' +
+      results[i].title + '" target="_blank">' + results[i].title +
+      '</a></div>';
+      var body = '';
+
+      $('.results').append('<div>' + title + '</div>');
+    }
+  }
+
 }
